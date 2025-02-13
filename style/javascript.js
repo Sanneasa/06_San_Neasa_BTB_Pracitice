441cc1a2c34c56d4582776a91107268c6a05ae36
SAN NEASA <sanneasa@SANs-MacBook-Pro.local>
2025-02-14 06:15:41 +0700

    secend

--- style/javascript.js
+++ style/javascript.js
@@ -1,1 +1,29 @@
-const  dropdown = document.getElementById("")
+let inputype = document.getElementById("search-dropdown");
+// call quote title
+
+// Get user input
+function myfunction(value) {
+  console.log(value);
+  if (value ==="Quoted id") {
+    console.log(value)
+    inputype.type = "number";
+    inputype.removeAttribute("disabled");
+    //   Remove disable attribute
+  } else if (value === "Quoted title") {
+    inputype.type = "text";
+    inputype.removeAttribute("disabled");
+  } else {
+  }
+}
+
+function handleSearch() {
+  console.log(inputype.value);
+  if(inputype.value == 1) {
+    // display img 
+    // img.src = ""
+    // display content
+
+    // display title
+    // quoute.innerHtml = "It's okay"
+  }
+}
